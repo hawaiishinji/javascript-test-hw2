52307dd469b1ba77e0030f0af8b42bc52026a5e1
Hawaii Ho <hawaiishinji@gmail.com>
2017-09-23 20:21:20 +0800

    refactor some logic to different function to make it more clear

--- src/js/app.js
+++ src/js/app.js
@@ -5,23 +5,32 @@ require('../css/custom.css');
 $(document).ready(function () {
 
   var discountRate = [0, 0.05, 0.1, 0.2, 0.25];
-  var cart = [];
 
   $('input[name=qty]').on('change', function () {
-    cart = [];
     handleInputChange();
   });
 
   function handleInputChange(){
+
+    const cart = getCart();
+    var totalPrice = cart.reduce((s, i) => s += 100 * i.qty, 0);
+    var uniqueQty = cart.reduce((s, i) => s += Math.min(1, i.qty), 0);
+    var discount = discountRate[uniqueQty-1] * uniqueQty * 100;
+    setPriceToUI(totalPrice - discount);
+  }
+
+  function setPriceToUI(price){
+    $('#price').text(price);
+  }
+
+  function getCart(){
+    var cart = [];
     $('.product').each(function () {
       var qty = $(this).find('input').val();
       cart.push({
         qty: qty
       })
     });
-    var totalPrice = cart.reduce((s, i) => s += 100 * i.qty, 0);
-    var uniqueQty = cart.reduce((s, i) => s += Math.min(1, i.qty), 0);
-    var discount = discountRate[uniqueQty-1] * uniqueQty * 100;
-    $('#price').text(totalPrice - discount);
+    return cart;
   }
 });
