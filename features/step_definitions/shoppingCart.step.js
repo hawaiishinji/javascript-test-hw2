8f2e30bf479c188c015f6651d52a6441ac2fd69a
Hawaii Ho <hawaiishinji@gmail.com>
2017-09-23 19:14:46 +0800

    npm run test to generate test code and write the test code by nightmare

--- features/step_definitions/shoppingCart.step.js
+++ features/step_definitions/shoppingCart.step.js
@@ -3,5 +3,26 @@ var chai = require('chai');
 chai.should();
 
 defineSupportCode(function ({ Given, When, Then }) {
+  var url = "http://localhost:8080"
+  Given('進入購物車頁面', async function () {
+    await this.driver.goto(url)
+          .viewport(1000, 760);
+  });
 
+  Given('選擇第 {string} 集，數量 {string} 一本', async function (index, qty) {
+    await this.driver
+        .insert(`div.product:nth-child(${index}) input[name=qty]`, false)
+        .insert(`div.product:nth-child(${index}) input[name=qty]`, +qty);
+  });
+
+  When('選擇完畢，計算價格', async function () {
+    var price = await this.driver
+        .evaluate(() => document.querySelector('#price').innerText);
+
+    this.actual = +price;
+  });
+
+  Then('折扣後價格為 {string} 元', async function (expected) {
+    this.actual.should.equal(+expected);
+  });
 })
